16d298c3f85b0eb7757cc89542aa287ac8bb80a9
pashioya <paul.ashioya@student.kdg.be>
2022-09-24 23:29:16 +0200

    Added Search Product functionality

--- productList.js
+++ productList.js
@@ -3,31 +3,33 @@ const axios = require("axios");
 // import params from "./params.json";
 // const params =  JSON.parse(params);
 
-// set up the request parameters should be in the format
-// const params = {
-//     api_key: "66263EFAEB4743E6B2AAB46DDBBD3364",
-//     type: "search",
-//     amazon_domain: "AMAZON_DOMAIN / amazon.(countryCode)",
-//     search_term: "SEARCH_TERM",
-//     sort_by: "SORT_PREFERENCES",
-// };
-
+/*
+set up the request parameters should be in the format
 const params = {
     api_key: "66263EFAEB4743E6B2AAB46DDBBD3364",
+    type: "search",
+    amazon_domain: "AMAZON_DOMAIN / amazon.(countryCode)",
+    search_term: "SEARCH_TERM",
+    sort_by: "SORT_PREFERENCES",
+};
+*/
+
+const params = {
+    api_key: "EBB9CA733E4F49F38A71ED2E7A4062AD",
     type: "search",
     amazon_domain: "amazon.de",
     search_term: "macbook",
 };
 
 const products = [];
+var chosenProduct;
+
 // make the http GET request to Rainforest API
 axios
     .get("https://api.rainforestapi.com/request", { params })
     .then((response) => {
         // print the JSON response from Rainforest API
         // const object = JSON.stringify(response.data, 0, 2);
-
-        // console.log(response.data.search_results);
 
         // for each product in the response data add the title, image and price to a object named data
         response.data.search_results.forEach((product) => {
@@ -37,7 +39,7 @@ axios
             }
 
             const data = {
-                id: product.position,
+                asin: product.asin,
                 title: product.title,
                 image: product.image,
                 price: product.price.value,
@@ -46,12 +48,55 @@ axios
             products.push(data);
         });
 
-        // print the array of products
-        console.log(products);
+        // print array of products
 
+        console.log(products);
         // export the array of products
+        exports.products = products;
     })
     .catch((error) => {
         // catch and print the error
         console.log(error);
     });
+
+// function to search for a product
+async function searchProduct(asin) {
+    const params = {
+        api_key: "EBB9CA733E4F49F38A71ED2E7A4062AD",
+        type: "product",
+        amazon_domain: "amazon.de",
+        asin: asin,
+    };
+
+    // make the http GET request to Rainforest API
+    axios
+        .get("https://api.rainforestapi.com/request", { params })
+        .then((response) => {
+            // print the JSON response from Rainforest API
+            // console.log(JSON.stringify(response.data, 0, 2));
+            // console.log(response.data);
+
+            const product = {
+                brand: response.data.product.brand,
+                description: response.data.product.description,
+                asin: response.data.product.asin,
+            };
+            // find the product in the array of products using the asin and add the brand and description to the product
+            var chosenProduct;
+            products.forEach((product) => {
+                if (product.asin == asin) {
+                    chosenProduct = product;
+
+                    chosenProduct.brand = response.data.product.brand;
+                    chosenProduct.description =
+                        response.data.product.description;
+                    console.log(chosenProduct);
+                }
+            });
+            // console.log(response);
+        })
+        .catch((error) => {
+            // catch and print the error
+            console.log(error);
+        });
+}
